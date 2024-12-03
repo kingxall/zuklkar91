854b5efc6281a0ccbf8ff79415929474fa9d20c7
yericoramadhani <yerico378@gmail.com>
2024-12-03 12:04:41 +0700

    Add files via upload

---
+++ produk.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require "navbar.php" ?>
+<!--banner-->
+    <div class="comtainer-fluid banner-produk">
+    </div class="container"></div>
+</div>
+
+</body>
+</html>
